2a78fd67dbf9e4c4fe8a8c93512993d0eb036b37
thaivm-0609 <vuongthai95@gmail.com>
2025-07-18 10:25:49 +0700

    feat: add animation and input system

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,53 +1,31 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    public float Speed = 5f;
-    Animator animator;
-    CharacterController controller;
-
+    Animator animator; //khai bao animator
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //su kien nhay
+        //khi ng dung bam phim space => kich hoat animation jump
         if (Input.GetKeyDown(KeyCode.Space))
         {
             animator.SetTrigger("Jump");
         }
 
-        //su kien nam xuong
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+        if (Input.GetKey(KeyCode.W))
         {
-            animator.SetBool("isKnockdown", true);
+            animator.SetBool("isRunning", true); //khi bam nut w chuyen trang thai running
         } else
         {
-            animator.SetBool("isKnockdown", false);
+            animator.SetBool("isRunning", false); //ngc lai thi chuyen ve trang thai idle
         }
-
-        //di chuyen 4 huong
-        float h = Input.GetAxisRaw("Horizontal"); // A/D
-        float v = Input.GetAxisRaw("Vertical");   // W/S
-
-        Vector3 moveDir = new Vector3(h, 0, v).normalized;
-
-        // xoay huong
-        if (moveDir.magnitude > 0.1f)
-        {
-            transform.forward = moveDir; // Xoay nhan vat 
-            controller.Move(moveDir * Speed * Time.deltaTime);
-        }
-
-        // Them speed
-        animator.SetFloat("Speed", moveDir.magnitude);
     }
 }
